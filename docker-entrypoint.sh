b66e6e211e07aa17f65319a50d83225f74f9896c
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-11-10 14:03:36 +0100

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ then
   SECRET=$(cat /dev/urandom | fold -w 128 | md5 | fold -w 16 | head -n 1)
 
   cat > mosquitto/config/mosquitto.conf << EOF
-plugin /opt/mosquitto-ip-jwt-auth/ip-jwt-auth.so
+plugin /ip-jwt-auth.so
 
 plugin_opt_jwt_signing_alg HS512
 plugin_opt_jwt_secret_key ${SECRET}
