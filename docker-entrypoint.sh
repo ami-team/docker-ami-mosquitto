60960910c06e53b9d46b173dfb81f73c7883cca8
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-11-10 14:14:53 +0100

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,14 @@ set -e
 
 if [ ! -f /mosquitto/config/mosquitto.conf ]
 then
-  SECRET=$(cat /dev/urandom | fold -w 128 | md5 | fold -w 16 | head -n 1)
+  RAND_SECRET=$(cat /dev/urandom | fold -w 128 | md5 | fold -w 16 | head -n 1)
 
   cat > mosquitto/config/mosquitto.conf << EOF
 plugin /ip-jwt-auth.so
 
 plugin_opt_jwt_signing_alg HS512
-plugin_opt_jwt_secret_key ${SECRET}
-plugin_opt_jwt_issuer AMI
+plugin_opt_jwt_secret_key ${JWT_SECRET:-${RAND_SECRET}}
+plugin_opt_jwt_issuer ${JWT_ISSUER:-AMI}
 
 listener 1883
 protocol mqtt
