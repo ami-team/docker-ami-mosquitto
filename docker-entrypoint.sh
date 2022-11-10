3ee1a27658ded9c33f923b432ba36fa72e13be12
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-11-10 14:02:56 +0100

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,25 @@ set -e
 
 if [ ! -f /mosquitto/config/mosquitto.conf ]
 then
-	cp /mosquitto/mosquitto.conf.orig /mosquitto/config/mosquitto.conf
+  SECRET=$(cat /dev/urandom | fold -w 128 | md5 | fold -w 16 | head -n 1)
+
+  cat > mosquitto/config/mosquitto.conf << EOF
+plugin /opt/mosquitto-ip-jwt-auth/ip-jwt-auth.so
+
+plugin_opt_jwt_signing_alg HS512
+plugin_opt_jwt_secret_key ${SECRET}
+plugin_opt_jwt_issuer AMI
+
+listener 1883
+protocol mqtt
+
+listener 9001
+protocol websockets
+
+allow_anonymous false
+
+$(cat /mosquitto/mosquitto.conf.orig)
+EOF
 fi
 
 # Set permissions
